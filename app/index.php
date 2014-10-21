3513a8228efee1394a65c25d85e1d8ea19887a07
Christopher Bartlett <christopher@seedindustries.com>
2014-10-21 13:55:52 +1100

    Initial Commit
    - index page frames
    - deals page
    - deals faqs
    - projects
    - navigatio

---
+++ app/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" href="./include/style.css">
+</head>
+<body>
+	<div class="header">
+		<div class="logo"></div>
+		<div class="profiler"></div>
+	</div>
+		<div class="interactions">
+			<div class="actions">
+
+			</div>
+			<div class="view">
+
+			</div>
+		</div>
+	<div class="agendr">
+
+		<div class="timeline">
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+				<div class="instance-node">
+					<div class="instance-time">
+						11:59 PM
+					</div>
+					<div class="instance-node-outer">
+						<div class="instance-node-inner">
+						</div>
+					</div>
+				</div>
+				<div class="instance">
+					<div class="instance-mark">
+					</div>
+				</div>
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+			<div class="timeline-item">
+			</div>
+		</div>
+		<div class="feed">
+		</div>		
+	</div>
+
+</body>
+<script src="./include/classes.js"></script>
